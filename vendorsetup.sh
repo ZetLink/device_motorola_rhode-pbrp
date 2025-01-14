ac31af4db031a23f1728059ab33ae0334a7b7326
ZetLink <royalezetlink@gmail.com>
2025-01-14 15:57:44 -0300

    rhode: fix path kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
 # Clone kernel tree
-git clone --depth=1 -b lineage-21 https://github.com/LineageOS/android_kernel_motorola_sm6225.git kernel/motorola/rhode
+git clone --depth=1 -b lineage-21 https://github.com/LineageOS/android_kernel_motorola_sm6225.git kernel/motorola/sm6225
